9ffa6c786d952773a9aa699ff07ffc57a5cc797c
Ivan <qyber1@yandex.ru>
2024-02-08 21:28:14 +0300

    Add new DB Model and insert default values

--- utils/db/engine.py
+++ utils/db/engine.py
@@ -1,24 +1,26 @@
 from sqlalchemy.ext.asyncio import AsyncEngine, AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
-from .models import BaseModel
-
-
+from .models import BaseModel, UserAdmin
 
 
 async def proseed_schemas(engine: AsyncEngine, metadata=BaseModel.metadata):
     async with engine.begin() as conn:
         await conn.run_sync(metadata.create_all)
 
+        async with AsyncSession(bind=conn) as session:
+            async with session.begin():
+                user_admin = UserAdmin()
+                session.add(user_admin)
+
 
 def session_maker(engine: AsyncEngine) -> sessionmaker:
-    return sessionmaker(engine, class_=AsyncSession)
+    sessionmaker(engine, class_=AsyncSession)
 
 
 class DBConnect:
 
      def __init__(self, url):
         self.engine = create_async_engine(url=url, echo=True, pool_pre_ping=True)
-
 
 
 def get_engine(url):
--- utils/db/models.py
+++ utils/db/models.py
@@ -1,11 +1,13 @@
 import datetime
 
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
 from sqlalchemy import String, ForeignKey, Time, Date
 
 
 class BaseModel(DeclarativeBase):
     pass
+
 
 class Department(BaseModel):
     __tablename__ = "Department"
@@ -25,6 +27,7 @@ class User(BaseModel):
     department: Mapped["Department"] = relationship(back_populates="employers")
     worklog: Mapped[list["Worklog"]] = relationship(back_populates='user')
 
+
 class Worklog(BaseModel):
     __tablename__ = "Worklog"
 
@@ -32,9 +35,19 @@ class Worklog(BaseModel):
     start_time: Mapped[datetime.datetime] = mapped_column(Time)
     end_time: Mapped[datetime.datetime] = mapped_column(Time, nullable=True, default=None)
     date: Mapped[datetime.datetime] = mapped_column(Date)
-    user_id: Mapped[int] =  mapped_column(ForeignKey("User.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("User.id"))
     comment: Mapped[str] = mapped_column(String(256), nullable=True)
     user: Mapped["User"] = relationship(back_populates='worklog')
 
+    @hybrid_property
+    def time_work(self):
+        return self.end_time - self.start_time
 
 
+class UserAdmin(BaseModel):
+    __tablename__ = 'UserAdmin'
+
+    id: Mapped[int] = mapped_column(unique=True, primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(String(30), server_default="admin")
+    password: Mapped[str] = mapped_column(String(256),
+                                          server_default="$2b$12$ZqgR3wQovAt.E9CaNXN/EevdYxBKao5NXqR8MfZu.IYKSj7ylejnC")
